42175d60f671f78ea526e80d34c6f7c54cf333b1
vishenosik <nik.kuzya192@mail.ru>
2025-04-16 23:35:17 +0400

    chore(pipeline): Merge multiple files into one.

--- pipeline.go
+++ pipeline.go
@@ -145,3 +145,49 @@ func (pln *pipeline[Type]) step(in <-chan Type, handler handlerFunc[Type]) <-cha
 	}()
 	return out
 }
+
+type Recoverer interface {
+	Recover(message any)
+}
+
+type noopRecoveryHandler struct{}
+
+func (h *noopRecoveryHandler) Recover(_ any) {}
+
+func (pln *pipeline[Type]) _recover() {
+	if !pln.recoverPanics {
+		return
+	}
+	if r := recover(); r != nil {
+		pln.recoverer.Recover(r)
+	}
+}
+
+func (pln *pipeline[Type]) SetSteps(steps ...handlerFunc[Type]) *pipeline[Type] {
+	pln.steps = steps
+	return pln
+}
+
+func (pln *pipeline[Type]) SetCommit(commit func(Type)) *pipeline[Type] {
+	pln.commit = commit
+	return pln
+}
+
+func (pln *pipeline[Type]) SetGoroutinesLimit(limit int) *pipeline[Type] {
+	if limit > 0 {
+		pln.goroutinesLimit = limit
+	}
+	return pln
+}
+
+func (pln *pipeline[Type]) DisableStepPanicRecover() *pipeline[Type] {
+	pln.recoverPanics = false
+	return pln
+}
+
+func (pln *pipeline[Type]) SetRecoveryHandler(handler Recoverer) *pipeline[Type] {
+	if handler != nil {
+		pln.recoverer = handler
+	}
+	return pln
+}
